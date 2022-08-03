603a1b0ebe4feb54c469eeff423e155902b97a52
alan-martinez <alan.martinez@alumnos.udg.mx>
2022-08-03 21:20:27 +0000

    Sockets conexion entre clientes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import {useState, useEffect} from 'react';
+
+const socket = io('http://localhost:4000')
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('message', message);
+    setMessage('');
+  }
+
+  useEffect(() => {
+    const receiveMesagge = message => {
+      console.log(message);
+    };
+
+    socket.on('message', receiveMesagge);
+  
+    return () => {
+      socket.off('message', receiveMesagge);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={e => setMessage(e.target.value)} value={message} />
+        <button>Send</button>
+      </form>
     </div>
   );
 }
