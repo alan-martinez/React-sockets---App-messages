d133e9374320ce224d488ba0c0e91f5a1b37d1d1
alan-martinez <alan.martinez@alumnos.udg.mx>
2022-08-03 22:03:55 +0000

    Frontend estilos

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ const socket = io('http://localhost:4000')
 
 function App() {
   const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     socket.emit('message', message);
+    const newMessage = {
+      body: message,
+      from: 'Me'
+    }
+    setMessages([newMessage, ...messages])
     setMessage('');
   }
 
   useEffect(() => {
     const receiveMesagge = message => {
-      console.log(message);
+      setMessages ([message, ...messages]);
     };
 
     socket.on('message', receiveMesagge);
@@ -23,13 +29,20 @@ function App() {
     return () => {
       socket.off('message', receiveMesagge);
     }
-  }, []);
+  }, [messages]);
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setMessage(e.target.value)} value={message} />
-        <button>Send</button>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+      <h1 className='text-2x1 font-bold my-2'>Chat React - Alan</h1>
+        <input type="text" onChange={e => setMessage(e.target.value)} value={message} className="border-2 border-zinc-500 p-2 text-black w-full"/>
+      <ul className='h-80 overflow-y-auto'>
+        {messages.map((message, index) => (
+          <li key={index} className={`my-2 p-2 table text-sm rounded-md ${message.from === "Me" ? "bg-sky-700 ml-auto": "bg-black"}`}>
+            <p>{message.from}: {message.body}</p>
+          </li>
+        ))}
+      </ul>
       </form>
     </div>
   );
